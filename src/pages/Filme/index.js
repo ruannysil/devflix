ee6ec4f3f38732081e5ad82b3033bac486e1ec04
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2022-09-21 11:39:56 -0300

    criando um site de filme

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Filme() {
+    return (
+        <div>
+            <h1>BEM VINDO A PAGINAS DETALHES DO FILME</h1>
+        </div>
+    )
+}
+
+export default Filme;
